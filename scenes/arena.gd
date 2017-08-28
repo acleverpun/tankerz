9329b7b2c5b358b70f8753016a8e3a73800ab79f
Ross Hadden <rosshadden@gmail.com>
2017-08-28 16:53:46 -0400

    Tried to query point's collisions.

---
+++ scenes/arena.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var utils = preload("res://scripts/utils.gd")
+
+func _input(event):
+	if utils.isClick(event):
+		var world = get_world_2d()
+		var space = world.get_direct_space_state()
+		var pos = event.get_position()
+		print(space.intersect_point(pos))
