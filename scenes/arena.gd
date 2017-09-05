d0eac22fa14f5b28c0f969102cec626dfaddb0d9
Ross Hadden <rosshadden@gmail.com>
2017-09-05 16:31:32 -0400

    Quit it.

--- scenes/arena.gd
+++ scenes/arena.gd
@@ -4,7 +4,10 @@ onready var utils = preload("res://scripts/utils.gd")
 
 func _input(event):
 	if utils.isClick(event):
-		var world = get_world_2d()
-		var space = world.get_direct_space_state()
 		var pos = event.get_position()
+		var world = get_viewport().get_world_2d()
+		var space = world.get_direct_space_state()
+
 		print(space.intersect_point(pos))
+	elif Input.is_action_just_pressed("quit"):
+		get_tree().quit()
