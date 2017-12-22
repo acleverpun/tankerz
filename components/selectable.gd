35fee9cf62b89478f2cc14d178972038edfde0ec
Ross Hadden <rosshadden@gmail.com>
2017-12-22 15:46:17 -0500

    Colored selected nodes.

--- components/selectable.gd
+++ components/selectable.gd
@@ -4,23 +4,29 @@ signal select
 signal deselect
 
 export var selected = false
+export(Color) var selectedColor
 
 onready var utils = preload('res://scripts/utils.gd')
 onready var parent = get_parent()
+onready var parentColor = parent.color
 
 func _ready():
+	if not selectedColor: selectedColor = parentColor.lightened(0.3)
 	if parent.has_method('onSelect'): self.connect('select', parent, 'onSelect')
 	if parent.has_method('onDeselect'): self.connect('deselect', parent, 'onDeselect')
 
 func select():
 	if not selected:
 		selected = true
+		parentColor = parent.color
+		parent.color = selectedColor
 		emit_signal('select')
 	return selected
 
 func deselect():
 	if selected:
 		selected = false
+		parent.color = parentColor
 		emit_signal('deselect')
 	return selected
 
